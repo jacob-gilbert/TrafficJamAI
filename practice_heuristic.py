9f1eb3bfda2f0b3b947e46267ba5bbd0270fd7e5
jacob-gilbert <jacob.gilbert@richmond.edu>
2024-12-30 15:12:47 -0500

    changed what lines of code were commented

--- practice_heuristic.py
+++ practice_heuristic.py
@@ -4,155 +4,6 @@ import move_the_obstacle_heuristic
 import line_heuristic
 import time
 
-initial_state = [   [0,             0,              0,              0,              0               ],
-                    [(1, "c", "h"), (1, "c", "h"),   0,             0,              0               ],
-                    [(2, "c", "v"), 0,              0,              (3, "c", "h"),  (3, "c", "h")   ],
-                    [(2, "c", "v"), (4, "c", "h"), (4, "c", "h"),  0,              (0, "c", "v")   ],
-                    [(5, "t", "h"), (5, "t", "h"),  (5, "t", "h"),  0,              (0, "c", "v")   ]
-                ]
-
-n = 5
-m = 5      
-goal = (0,4)          
-"""print("Test1")
-#print(manhattan_heuristic.astar_search(n, m, initial_state, goal))
-#print(traffic_jam.astar_search(n, m, initial_state, goal))
-path, g = move_the_obstacle_heuristic.astar_search(n, m, initial_state, goal)
-print(g)
-print(line_heuristic.astar_search(n, m, initial_state, goal))
-print()"""
-
-
-initial_state = [   [0,              0,              0               ],
-                    [0,              0,              0               ],
-                    [0,              (3, "c", "h"),  (3, "c", "h")   ],
-                    [(4, "c", "v"),  0,              (0, "c", "v")   ],
-                    [(4, "t", "v"),  0,              (0, "c", "v")   ]
-                ]
-
-n = 5
-m = 3      
-goal = (0,2)          
-"""print("Test2")
-print(manhattan_heuristic.astar_search(n, m, initial_state, goal))
-print(traffic_jam.astar_search(n, m, initial_state, goal))
-path, g = move_the_obstacle_heuristic.astar_search(n, m, initial_state, goal)
-print(g)
-print(line_heuristic.astar_search(n, m, initial_state, goal))
-print()"""
-
-
-initial_state = [   [0,  (3, "c", "h"),  (3, "c", "h")   ],
-                    [(4, "c", "v"),  0,              (0, "c", "v")   ],
-                    [(4, "c", "v"),  0,              (0, "c", "v")   ]
-                ]
-
-n = 3
-m = 3      
-goal = (0,2) 
-"""print("Test3")
-print(manhattan_heuristic.astar_search(n, m, initial_state, goal))
-print(traffic_jam.astar_search(n, m, initial_state, goal))
-path, g = move_the_obstacle_heuristic.astar_search(n, m, initial_state, goal)
-print(g)
-print(line_heuristic.astar_search(n, m, initial_state, goal))
-print()"""
-
-
-
-initial_state = [   [0,             0,              0,              (1, "t", "h"),  (1,"t","h"),    (1,"t","h")    ],
-                    [0,             0,              (2, "c", "v"),  0,              (0, "c", "v"),  0              ],
-                    [0,             0,              (2, "c", "v"),  0,              (0, "c", "v"),  0              ],
-                    [0,             0,              0,              0,               0,             0              ]
-                ]
-
-n = 4
-m = 6  
-goal = (0, 4)      
-        
-"""print("Test4")
-print(manhattan_heuristic.astar_search(n, m, initial_state, goal))
-print(traffic_jam.astar_search(n, m, initial_state, goal))
-path, g = move_the_obstacle_heuristic.astar_search(n, m, initial_state, goal)
-print(g)
-print(line_heuristic.astar_search(n, m, initial_state, goal))
-print()"""
-
-
-initial_state = [   [0,             0,              (3, "c", "h"),  (3, "c", "h"),  (1, "c", "h"),  (1, "c", "h")  ],
-                    [0,             0,              0,              0,              (0, "c", "v"),  0              ],
-                    [0,             0,              0,              0,              (0, "c", "v"),  0              ]
-                ]
-
-n = 3
-m = 6                
-goal = (0,4)
-
-"""print("Test5")
-print(manhattan_heuristic.astar_search(n, m, initial_state, goal))
-print(traffic_jam.astar_search(n, m, initial_state, goal))
-path, g = move_the_obstacle_heuristic.astar_search(n, m, initial_state, goal)
-print(g)
-print(line_heuristic.astar_search(n, m, initial_state, goal))
-print()"""
-
-
-initial_state = [   [0,             0,              (3, "c", "h"),  (3, "c", "h"),  (1, "c", "h"),  (1, "c", "h")  ],
-                    [0,             0,              (2, "c", "v"),  0,              (0, "c", "v"),  0              ],
-                    [0,             0,              (2, "c", "v"),  0,              (0, "c", "v"),  0              ],
-                    [0,             0,              0,              0,               0,             0              ]
-                ]
-
-n = 4
-m = 6                
-goal = (0,4)
-
-#print("Test6")
-#print(manhattan_heuristic.astar_search(n, m, initial_state, goal))
-#print(traffic_jam.astar_search(n, m, initial_state, goal))
-#path, g = move_the_obstacle_heuristic.astar_search(n, m, initial_state, goal)
-#print(g)
-#for state in path:
-#    move_the_obstacle_heuristic.print_traffic_jam(state)
-#    print("\n")
-#print(line_heuristic.astar_search(n, m, initial_state, goal))
-#print("\n")
-
-
-initial_state = [   [0,             0,              0,              (8, "t", "h"),  (8, "t", "h"),      (8, "t", "h")],
-                    [(1, "c", "h"), (1, "c", "h"),  0,              (2, "c", "h"),  (2, "c", "h"),      0],
-                    [(2, "c", "v"), (7, "c", "h"),  (7, "c", "h"),  0,              (0, "c", "v"),      0],
-                    [(2, "c", "v"), (4, "c", "h"),  (4, "c", "h"),  0,              (0, "c", "v"),      0],
-                    [(5, "t", "h"), (5, "t", "h"),  (5, "t", "h"),  0,              0,                  0 ]]
-n = 5
-m = 6
-goal = (0,4)
-
-"""print("Test7")
-#print(manhattan_heuristic.astar_search(n, m, initial_state, goal))
-#print(traffic_jam.astar_search(n, m, initial_state, goal))
-path, g = move_the_obstacle_heuristic.astar_search(n, m, initial_state, goal)
-print(g)
-print(line_heuristic.astar_search(n, m, initial_state, goal))
-print()"""
-
-
-initial_state = [   [0,             0,              (2, "t", "v"),  (1, "t", "h"),  (1, "t", "h"),  (1, "t", "h")],
-                    [0,             0,              (2, "t", "v"),  (7, "c", "v"),  (8, "c", "h"),  (8, "c", "h")],
-                    [0,             0,              (2, "t", "v"),  (7, "c", "v"),  0,              (0, "c", "v")],
-                    [0,             0,              (5, "t", "h"),  (5, "t", "h"),  (5, "t", "h"),  (0, "c", "v")],
-                    [0,             0,              0,              (6, "t", "h"),  (6, "t", "h"),  (6, "t", "h")],
-                    [0,             0,              0,              0,              0,              0            ]]
-#print("Test8")
-n = 6
-m = 6
-goal = (0, 5)
-#path, g = move_the_obstacle_heuristic.astar_search(n, m, initial_state, goal)
-#print(g)
-#print(line_heuristic.astar_search(n, m, initial_state, goal)) # passes
-
-
-
 initial_state = [   [0,             0,              0,              (4, "c", "v"),  (5, "c", "h"),  (5, "c", "h")],
                     [0,             0,              0,              (4, "c", "v"),  (2, "c", "h"),  (2, "c", "h")],
                     [(1, "c", "v"), (7, "t", "h"),  (7, "t", "h"),  (7, "t", "h"),  (3, "c", "h"),  (3, "c", "h")],
@@ -163,18 +14,18 @@ n = 6
 m = 6
 goal = (0,5)
 
-"""print("Park Test1")
+print("Hard Test1")
 start_time = time.time()
 print(manhattan_heuristic.astar_search(n, m, initial_state, goal))
 end_time = time.time()
 execution_time = end_time - start_time
-print(f"Execution time: {execution_time} seconds")"""
+print(f"Execution time: {execution_time} seconds")
 
-"""start_time = time.time()
+start_time = time.time()
 print(traffic_jam.astar_search(n, m, initial_state, goal))
 end_time = time.time()
 execution_time = end_time - start_time
-print(f"Execution time: {execution_time} seconds")"""
+print(f"Execution time: {execution_time} seconds")
 
 start_time = time.time()
 path, g = move_the_obstacle_heuristic.astar_search(n, m, initial_state, goal)
@@ -207,18 +58,18 @@ n = 6
 m = 6
 goal = (0,4)
 
-"""print("Park Test2")
+print("Hard Test2")
 start_time = time.time()
 print(manhattan_heuristic.astar_search(n, m, initial_state, goal))
 end_time = time.time()
 execution_time = end_time - start_time
-print(f"Execution time: {execution_time} seconds")"""
+print(f"Execution time: {execution_time} seconds")
 
-"""start_time = time.time()
+start_time = time.time()
 print(traffic_jam.astar_search(n, m, initial_state, goal))
 end_time = time.time()
 execution_time = end_time - start_time
-print(f"Execution time: {execution_time} seconds")"""
+print(f"Execution time: {execution_time} seconds")
 
 start_time = time.time()
 path, g = move_the_obstacle_heuristic.astar_search(n, m, initial_state, goal)
@@ -248,18 +99,18 @@ n = 6
 m = 6
 goal = (0,5)
 
-"""print("Park Test3")
+print("Hard Test3")
 start_time = time.time()
 print(manhattan_heuristic.astar_search(n, m, initial_state, goal))
 end_time = time.time()
 execution_time = end_time - start_time
-print(f"Execution time: {execution_time} seconds")"""
+print(f"Execution time: {execution_time} seconds")
 
-"""start_time = time.time()
+start_time = time.time()
 print(traffic_jam.astar_search(n, m, initial_state, goal))
 end_time = time.time()
 execution_time = end_time - start_time
-print(f"Execution time: {execution_time} seconds")"""
+print(f"Execution time: {execution_time} seconds")
 
 start_time = time.time()
 path, g = move_the_obstacle_heuristic.astar_search(n, m, initial_state, goal)
@@ -275,3 +126,151 @@ end_time = time.time()
 execution_time = end_time - start_time
 print(f"Execution time: {execution_time} seconds")
 print("\n")
+
+# the following lines are other test options
+"""initial_state = [   [0,             0,              0,              0,              0               ],
+                    [(1, "c", "h"), (1, "c", "h"),   0,             0,              0               ],
+                    [(2, "c", "v"), 0,              0,              (3, "c", "h"),  (3, "c", "h")   ],
+                    [(2, "c", "v"), (4, "c", "h"), (4, "c", "h"),  0,              (0, "c", "v")   ],
+                    [(5, "t", "h"), (5, "t", "h"),  (5, "t", "h"),  0,              (0, "c", "v")   ]
+                ]
+
+n = 5
+m = 5      
+goal = (0,4)          
+print("Test1")
+print(manhattan_heuristic.astar_search(n, m, initial_state, goal))
+print(traffic_jam.astar_search(n, m, initial_state, goal))
+path, g = move_the_obstacle_heuristic.astar_search(n, m, initial_state, goal)
+print(g)
+print(line_heuristic.astar_search(n, m, initial_state, goal))
+print()
+
+
+initial_state = [   [0,              0,              0               ],
+                    [0,              0,              0               ],
+                    [0,              (3, "c", "h"),  (3, "c", "h")   ],
+                    [(4, "c", "v"),  0,              (0, "c", "v")   ],
+                    [(4, "t", "v"),  0,              (0, "c", "v")   ]
+                ]
+
+n = 5
+m = 3      
+goal = (0,2)          
+print("Test2")
+print(manhattan_heuristic.astar_search(n, m, initial_state, goal))
+print(traffic_jam.astar_search(n, m, initial_state, goal))
+path, g = move_the_obstacle_heuristic.astar_search(n, m, initial_state, goal)
+print(g)
+print(line_heuristic.astar_search(n, m, initial_state, goal))
+print()
+
+
+initial_state = [   [0,  (3, "c", "h"),  (3, "c", "h")   ],
+                    [(4, "c", "v"),  0,              (0, "c", "v")   ],
+                    [(4, "c", "v"),  0,              (0, "c", "v")   ]
+                ]
+
+n = 3
+m = 3      
+goal = (0,2) 
+print("Test3")
+print(manhattan_heuristic.astar_search(n, m, initial_state, goal))
+print(traffic_jam.astar_search(n, m, initial_state, goal))
+path, g = move_the_obstacle_heuristic.astar_search(n, m, initial_state, goal)
+print(g)
+print(line_heuristic.astar_search(n, m, initial_state, goal))
+print()
+
+
+
+initial_state = [   [0,             0,              0,              (1, "t", "h"),  (1,"t","h"),    (1,"t","h")    ],
+                    [0,             0,              (2, "c", "v"),  0,              (0, "c", "v"),  0              ],
+                    [0,             0,              (2, "c", "v"),  0,              (0, "c", "v"),  0              ],
+                    [0,             0,              0,              0,               0,             0              ]
+                ]
+
+n = 4
+m = 6  
+goal = (0, 4)      
+        
+print("Test4")
+print(manhattan_heuristic.astar_search(n, m, initial_state, goal))
+print(traffic_jam.astar_search(n, m, initial_state, goal))
+path, g = move_the_obstacle_heuristic.astar_search(n, m, initial_state, goal)
+print(g)
+print(line_heuristic.astar_search(n, m, initial_state, goal))
+print()
+
+
+initial_state = [   [0,             0,              (3, "c", "h"),  (3, "c", "h"),  (1, "c", "h"),  (1, "c", "h")  ],
+                    [0,             0,              0,              0,              (0, "c", "v"),  0              ],
+                    [0,             0,              0,              0,              (0, "c", "v"),  0              ]
+                ]
+
+n = 3
+m = 6                
+goal = (0,4)
+
+print("Test5")
+print(manhattan_heuristic.astar_search(n, m, initial_state, goal))
+print(traffic_jam.astar_search(n, m, initial_state, goal))
+path, g = move_the_obstacle_heuristic.astar_search(n, m, initial_state, goal)
+print(g)
+print(line_heuristic.astar_search(n, m, initial_state, goal))
+print()
+
+
+initial_state = [   [0,             0,              (3, "c", "h"),  (3, "c", "h"),  (1, "c", "h"),  (1, "c", "h")  ],
+                    [0,             0,              (2, "c", "v"),  0,              (0, "c", "v"),  0              ],
+                    [0,             0,              (2, "c", "v"),  0,              (0, "c", "v"),  0              ],
+                    [0,             0,              0,              0,               0,             0              ]
+                ]
+
+n = 4
+m = 6                
+goal = (0,4)
+
+print("Test6")
+print(manhattan_heuristic.astar_search(n, m, initial_state, goal))
+print(traffic_jam.astar_search(n, m, initial_state, goal))
+path, g = move_the_obstacle_heuristic.astar_search(n, m, initial_state, goal)
+print(g)
+for state in path:
+    move_the_obstacle_heuristic.print_traffic_jam(state)
+    print("\n")
+print(line_heuristic.astar_search(n, m, initial_state, goal))
+print("\n")
+
+
+initial_state = [   [0,             0,              0,              (8, "t", "h"),  (8, "t", "h"),      (8, "t", "h")],
+                    [(1, "c", "h"), (1, "c", "h"),  0,              (2, "c", "h"),  (2, "c", "h"),      0],
+                    [(2, "c", "v"), (7, "c", "h"),  (7, "c", "h"),  0,              (0, "c", "v"),      0],
+                    [(2, "c", "v"), (4, "c", "h"),  (4, "c", "h"),  0,              (0, "c", "v"),      0],
+                    [(5, "t", "h"), (5, "t", "h"),  (5, "t", "h"),  0,              0,                  0 ]]
+n = 5
+m = 6
+goal = (0,4)
+
+print("Test7")
+print(manhattan_heuristic.astar_search(n, m, initial_state, goal))
+print(traffic_jam.astar_search(n, m, initial_state, goal))
+path, g = move_the_obstacle_heuristic.astar_search(n, m, initial_state, goal)
+print(g)
+print(line_heuristic.astar_search(n, m, initial_state, goal))
+print()
+
+
+initial_state = [   [0,             0,              (2, "t", "v"),  (1, "t", "h"),  (1, "t", "h"),  (1, "t", "h")],
+                    [0,             0,              (2, "t", "v"),  (7, "c", "v"),  (8, "c", "h"),  (8, "c", "h")],
+                    [0,             0,              (2, "t", "v"),  (7, "c", "v"),  0,              (0, "c", "v")],
+                    [0,             0,              (5, "t", "h"),  (5, "t", "h"),  (5, "t", "h"),  (0, "c", "v")],
+                    [0,             0,              0,              (6, "t", "h"),  (6, "t", "h"),  (6, "t", "h")],
+                    [0,             0,              0,              0,              0,              0            ]]
+print("Test8")
+n = 6
+m = 6
+goal = (0, 5)
+path, g = move_the_obstacle_heuristic.astar_search(n, m, initial_state, goal)
+print(g)
+print(line_heuristic.astar_search(n, m, initial_state, goal)) # passes"""
